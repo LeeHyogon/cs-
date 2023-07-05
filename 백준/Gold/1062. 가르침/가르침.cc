15156334b7c86f3d944ce29c37c210497bf5f092
LeeHyogon <45483116+LeeHyogon@users.noreply.github.com>
2023-07-05 19:23:38 +0900

    [Gold IV] Title: 가르침, Time: 24 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1062. 가르침/가르침.cc
+++ 백준/Gold/1062. 가르침/가르침.cc
@@ -1,81 +1,73 @@
-#include<iostream>
-#include<vector>
-#include<cstring>
-#include<queue>
-#include<stack>
-#include<algorithm>
-#include<string>
-#include<tuple>
-#include<cmath>
-#include<functional>
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <algorithm>
+#include <queue>
 using namespace std;
-#define ll long long
-#define pii pair<int,int>
-#define pll pair<ll,ll>
 #define INF 987654321
 #define LINF 321321321321
-#define MAX 100
 
-int N, K;
-char a[6] = "antic";
-char b[22] = "bdefghjklmopqrsuvwxyz";
-int str[100];
-int v[100];
-string s;
-int ans;
-void stob(int index) {
+int n, k;
+int ret = 0;
+
+int str[101];
+int v[101];
+string alpha = "bdefghjklmopqrsuvwxyz";
+
+void stob(string s, int index) {
+	int tmp = 0;
 	for (int i = 0; i < s.length(); i++) {
-		if (s[i] == 'a' || s[i] == 'n' || s[i] == 't' || s[i] == 'i' || s[i] == 'c') continue;
+		if (s[i] == 'a' || s[i] == 'n' || s[i] == 't' || s[i] == 'i' || s[i] == 'c')
+			continue;
 		for (int j = 0; j < 21; j++) {
-			if (s[i] == b[j]) {
-				str[index] |= 1 << j;
+			if (s[i] == alpha[j]) {
+				tmp |= 1 << j;
 			}
 		}
 	}
+	str[index] = tmp;
 }
 
-void picked(int cnt, int depth) {
-	
-	if (depth >= 22) return;
-	if (cnt == K - 5) {
-		int ret = 0;
+void dfs(int cnt, int cur) {
+
+	if (cur >= 22)
+		return;
+	if (cnt == k - 5) {
+
 		int tmp = 0;
 		for (int i = 0; i < cnt; i++) {
-			tmp |= 1 << v[i];
+			tmp |= (1 << v[i]);
 		}
-		for (int i = 0; i < N; i++) {
-			if (str[i] == (str[i] & tmp))
-				ret++;
+		int tcnt = 0;
+		for (int i = 0; i < n; i++) {
+			if (str[i] == (str[i] & tmp)) {
+				tcnt++;
+			}
 		}
-
-		ans = max(ans, ret);
+		ret = max(ret, tcnt);
 		return;
 	}
-	v[cnt] = depth;
-	picked(cnt + 1, depth + 1);
+	v[cnt] = cur;
+	dfs(cnt+1, cur + 1);
 	v[cnt] = 0;
-	picked(cnt, depth + 1);
-
+	dfs(cnt, cur + 1);
 }
-
-
-int main(void) {
-	std::ios::sync_with_stdio(false);
-	cin.tie(NULL);
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> n >> k;
 	
-	cin >> N >> K;
-	for (int i = 0; i < N; i++) {
+	for (int i = 0; i < n; i++) {
+		string s;
 		cin >> s;
-		stob(i);
+		stob(s, i);
 	}
-	if (K - 5 < 0) {
+	if (k - 5 < 0) {
 		cout << 0;
 		return 0;
 	}
-	
-	picked(0, 0);
-	cout << ans;
-	
+	dfs(0,0);
+	cout << ret;
 	return 0;
-
 }
+
