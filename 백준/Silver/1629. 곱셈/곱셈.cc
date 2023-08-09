db0775320d3c06923393cfb16c6658e7308e270e
LeeHyogon <45483116+LeeHyogon@users.noreply.github.com>
2023-08-09 22:06:05 +0900

    [Silver I] Title: 곱셈, Time: 64 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <algorithm>
+#include <functional>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <sstream>
+#include <stack>
+#include <queue>
+using namespace std;
+#define INF 987654321
+#define LINF 321321321321
+typedef long long ll;
+typedef pair<int, int > pii;
+
+ll A, B, C;
+
+ll Pow(ll A, ll B) {
+
+	if (B == 0)
+		return 1;
+	if (B == 1) {
+		return A % C;
+	}
+	if (B % 2 == 1) {
+		return ((Pow(A, B / 2) % C) * (Pow(A, B / 2 + 1) % C)) % C;
+	}
+	else
+		return ((Pow(A, B / 2) % C) * (Pow(A, B / 2) % C)) % C;
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	cin >> A >> B >> C;
+
+
+	cout << Pow(A, B) % C;
+
+
+	return 0;
+}
+
