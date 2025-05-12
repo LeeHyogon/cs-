c89ba5966879cb1bd47e316ff0051fa87af0ce77
LeeHyogon <45483116+LeeHyogon@users.noreply.github.com>
2025-05-12 20:19:18 +0900

    [Silver I] Title: 행운의 문자열, Time: 4140 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1342. 행운의 문자열/행운의 문자열.py
+++ 백준/Silver/1342. 행운의 문자열/행운의 문자열.py
@@ -1,45 +1,32 @@
-import sys
-sys.setrecursionlimit(10**6)
-
 string = input()
 N = len(string)
+string = sorted(string)  # 중복 제거를 위한 정렬
 
 visited = [False] * N
-
 answer = 0
-rec = set()
 
-def luck_str(str):
-
-    for i in range(len(str)-1):
-        if str[i] == str[i+1]:
-            return False
-    return True
-
-def dfs (str, visited) :
+def dfs(curr_str):
     global answer
-    
-    if str in rec:
+
+    if len(curr_str) == N:
+        answer += 1
         return
 
-    if len(str) == N:
-        rec.add(str)
-
-        answer+=1
-        return
-
+    prev_char = ''  # 이번 dfs 깊이에서 이전에 사용한 문자
     for i in range(N):
-        if visited[i] == False:
-            if str and str[-1] == string[i]:
-                continue
+        if visited[i]:
+            continue
 
-            visited[i] = True
-            dfs(str+string[i],visited)
-            visited[i] = False
+        # 동일한 문자 중복 사용 방지 + 인접 문자 체크
+        if string[i] == prev_char:
+            continue
+        if curr_str and curr_str[-1] == string[i]:
+            continue
 
-    return
+        prev_char = string[i]
+        visited[i] = True
+        dfs(curr_str + string[i])
+        visited[i] = False
 
-dfs("",visited)
-
+dfs("")
 print(answer)
-
