dcd55b5b010b734cbaf36232f1c13cc0308ad579
LeeHyogon <45483116+LeeHyogon@users.noreply.github.com>
2025-09-29 20:10:30 +0900

    [Silver III] Title: 달팽이, Time: 832 ms, Memory: 70928 KB -BaekjoonHub

--- 백준/Silver/1913. 달팽이/달팽이.py
+++ 백준/Silver/1913. 달팽이/달팽이.py
@@ -1,6 +1,3 @@
-import sys
-sys.setrecursionlimit(10**6)
-
 N = int(input())
 target = int (input())
 
@@ -9,29 +6,41 @@ dx = [0,1,0,-1]
 
 board = [[0]*N for _ in range(N)]
 
-def dfs (y, x , dir, value):
+value = N*N
+
+y = 0
+x = 0
+
+ans_y =0
+ans_x =0
+dir = 0
+while 1:
+    if value == target:
+        ans_y = y+1
+        ans_x = x+1
+    
+    if value == 1:
+        board[y][x] = value
+        break
+
     board[y][x] = value
-
+    
     ny = y + dy[dir]
-    nx = x + dx[dir]
-
+    nx = x + dx[dir]    
+    
     if 0<=ny <N and 0<=nx<N and board[ny][nx] == 0:
-        dfs(ny,nx,dir,value-1)
+        y = ny
+        x = nx
+        dir = dir
+        value -=1
     else:
         dir = (dir+1)%4
-        ny = y + dy[dir]
-        nx = x + dx[dir]
-        if board[ny][nx] == 0:
-            dfs(ny,nx,dir,value-1)
-
-
-dfs(0,0,0,N*N)
+        y = y + dy[dir]
+        x = x + dx[dir]        
+        value = value -1
+            
 
 for row in board:
     print(" ".join(map(str, row)))
 
-
-for i, row in enumerate(board):
-    for j,col in enumerate(row):
-        if col == target:
-            print(i+1, j+1)
+print(ans_y, ans_x)
